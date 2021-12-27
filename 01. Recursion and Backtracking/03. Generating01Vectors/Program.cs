67e11cd58db6705c75c1080eb41469b88d569e56
BorisLechev <b.k.lechev@gmail.com>
2021-12-27 23:11:41 +0200

    Add 03. Generating01Vectors

---
+++ 01. Recursion and Backtracking/03. Generating01Vectors/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _03._Generating01Vectors
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var vector = new int[n];
+
+            Generate01(vector, 0);
+        }
+
+        private static void Generate01(int[] vector, int index)
+        {
+            if (index == vector.Length)
+            {
+                Console.WriteLine(String.Join("", vector));
+                return;
+            }
+
+            for (int number = 0; number <= 1; number++)
+            {
+                vector[index] = number;
+
+                Generate01(vector, index + 1);
+            }
+        }
+    }
+}
