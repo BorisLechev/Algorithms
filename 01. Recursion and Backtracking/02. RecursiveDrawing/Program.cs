c4156c811170f445d345730f4eb2a79420800488
BorisLechev <b.k.lechev@gmail.com>
2021-12-27 22:42:49 +0200

    Add 02. RecursiveDrawing

---
+++ 01. Recursion and Backtracking/02. RecursiveDrawing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02._RecursiveDrawing
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            DrawFigure(n);
+        }
+
+        private static void DrawFigure(int size)
+        {
+            if (size == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*', size));
+
+            DrawFigure(size - 1);
+
+            Console.WriteLine(new string('#', size));
+        }
+    }
+}
