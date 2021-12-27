6644c1aff4ad022da243b2639a8aa2bef285b2fc
BorisLechev <b.k.lechev@gmail.com>
2021-12-27 23:22:38 +0200

    03. RecursiveFactorial

---
+++ 01. Recursion and Backtracking/04. RecursiveFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04._RecursiveFactorial
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CalculateFactorial(n));
+        }
+
+        private static int CalculateFactorial(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return n * CalculateFactorial(n - 1);
+        }
+    }
+}
