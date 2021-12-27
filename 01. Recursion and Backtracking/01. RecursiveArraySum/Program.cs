d25a2b3375d555da6e7ec07de0c29e94bf345f70
BorisLechev <b.k.lechev@gmail.com>
2021-12-27 22:30:44 +0200

    Add 01. RecursiveArraySum

---
+++ 01. Recursion and Backtracking/01. RecursiveArraySum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._RecursiveArraySum
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var array = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+
+            int index = 0;
+
+            Console.WriteLine(CalcSum(array, index));
+        }
+
+        private static int CalcSum(int[] array, int index)
+        {
+            if (index == array.Length - 1)
+            {
+                return array[index];
+            }
+
+            return array[index] + CalcSum(array, index + 1);
+        }
+    }
+}
