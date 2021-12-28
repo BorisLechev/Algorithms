15b2355cac7ad7f547621573e45161e5b304d912
BorisLechev <b.k.lechev@gmail.com>
2021-12-28 14:52:45 +0200

    Add 07. RecursiveFibonacci

---
+++ 01. Recursion and Backtracking/07. RecursiveFibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07._RecursiveFibonacci
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+            var fibonacci = GetFibonacci(number);
+
+            Console.WriteLine(fibonacci);
+        }
+
+        private static int GetFibonacci(int number)
+        {
+            if (number <= 1)
+            {
+                return 1;
+            }
+
+            return GetFibonacci(number - 1) + GetFibonacci(number - 2);
+        }
+    }
+}
