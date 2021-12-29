3d4ac64d6c74cddcef82477a4d38333ef7faa1d3
BorisLechev <b.k.lechev@gmail.com>
2021-12-29 18:19:13 +0200

    Add 07. PascalsTriangle.

---
+++ 02. Combinatorial Problems/07. PascalsTriangle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07._PascalsTriangle
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var rows = int.Parse(Console.ReadLine());
+            var cols = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetBinom(rows, cols));
+        }
+
+        private static int GetBinom(int row, int col)
+        {
+            if (row <= 1 || col == 0 || row == col)
+            {
+                return 1;
+            }
+
+            return GetBinom(row - 1, col - 1) + GetBinom(row - 1, col);
+        }
+    }
+}
