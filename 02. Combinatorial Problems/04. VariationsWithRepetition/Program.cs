402c583215f799c47984999ddc5ead3d9f80acdf
BorisLechev <b.k.lechev@gmail.com>
2021-12-29 16:15:08 +0200

    Add 04. variationsWithRepetition

---
+++ 02. Combinatorial Problems/04. VariationsWithRepetition/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._VariationsWithRepetition
+{
+    public class Program
+    {
+        private static string[] inputElements;
+        private static int slotsToFill;
+        private static string[] variations;
+
+        public static void Main()
+        {
+            inputElements = Console.ReadLine()
+                            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                            .ToArray();
+            slotsToFill = int.Parse(Console.ReadLine());
+
+            variations = new string[slotsToFill];
+
+            FindVariations(0);
+        }
+
+        private static void FindVariations(int variationIndex)
+        {
+            if (variationIndex >= variations.Length)
+            {
+                Console.WriteLine(String.Join(" ", variations));
+                return;
+            }
+
+            for (int elementIndex = 0; elementIndex < inputElements.Length; elementIndex++)
+            {
+                variations[variationIndex] = inputElements[elementIndex];
+
+                FindVariations(variationIndex + 1);
+            }
+        }
+    }
+}
