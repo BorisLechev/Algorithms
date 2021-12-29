03a582140c013f17e06a829089d1d4374b9180e5
BorisLechev <b.k.lechev@gmail.com>
2021-12-29 18:03:17 +0200

    Add 06. CombinationsWithRepetition.

---
+++ 02. Combinatorial Problems/06. CombinationsWithRepitition/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace _06._CombinationsWithRepitition
+{
+    public class Program
+    {
+        private static string[] inputElements;
+        private static int slotsToFill;
+        private static string[] combinations;
+
+        public static void Main()
+        {
+            inputElements = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            slotsToFill = int.Parse(Console.ReadLine());
+            combinations = new string[slotsToFill];
+
+            FindCombinations(0, 0);
+        }
+
+        private static void FindCombinations(int combinationIndex, int inputElementsStartIndex)
+        {
+            if (combinationIndex >= combinations.Length)
+            {
+                Console.WriteLine(String.Join(" ", combinations));
+                return;
+            }
+
+            for (int elementIndex = inputElementsStartIndex; elementIndex < inputElements.Length; elementIndex++)
+            {
+                combinations[combinationIndex] = inputElements[elementIndex];
+
+                FindCombinations(combinationIndex + 1, elementIndex);
+            }
+        }
+    }
+}
