59743b374536bb1fb4da78138595f21cd36d8f31
BorisLechev <b.k.lechev@gmail.com>
2022-02-07 15:03:11 +0200

    Add 01. ReverseArrayWithoutRecursion01.

---
+++ 03. Exercise Recursion and Combinatorial Algo/03. Exercise/01. ReverseArrayWithoutRecursionOne/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._ReverseArrayWithoutRecursionOne
+{
+    public class Program
+    {
+        private static string[] array;
+
+        public static void Main()
+        {
+            array = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            for (int left = 0; left < array.Length / 2; left++)
+            {
+                var right = array.Length - 1 - left;
+
+                SwapElements(left, right);
+            }
+
+            Console.WriteLine(String.Join(" ", array));
+        }
+
+        private static void SwapElements(int firstIndex, int secondIndex)
+        {
+            var temp = array[firstIndex];
+
+            array[firstIndex] = array[secondIndex];
+            array[secondIndex] = temp;
+        }
+    }
+}
