4f7d02bca7bedc7e2f6d5f36fdf4fba6766d01b0
BorisLechev <b.k.lechev@gmail.com>
2022-02-07 16:34:10 +0200

    Add 02. NestedLoopsToRecursion.

---
+++ 03. Exercise Recursion and Combinatorial Algo/03. Exercise/02. NestedLoopsToRecursion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _02._NestedLoopsToRecursion
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var elementsCount = int.Parse(Console.ReadLine());
+
+            var elements = new int[elementsCount];
+
+            Loop(elements, 0);
+        }
+
+        private static void Loop(int[] elements, int index)
+        {
+            if (index == elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", elements));
+                return;
+            }
+
+            for (int number = 1; number < elements.Length + 1; number++)
+            {
+                elements[index] = number;
+
+                Loop(elements, index + 1);
+            }
+        }
+    }
+}
