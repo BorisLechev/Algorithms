e2256c6c5d6cb9e21336ce74063e3942d4205022
BorisLechev <b.k.lechev@gmail.com>
2022-02-07 19:48:04 +0200

    Add 04. CombinationsWithoutRepetition.

---
+++ 03. Exercise Recursion and Combinatorial Algo/03. Exercise/04. CombinationsWithoutRepetition/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _04._CombinationsWithoutRepetition
+{
+    public class Program
+    {
+        private static int numberOfElements;
+
+        private static int slotsToFill;
+
+        private static int[] combinations;
+
+        public static void Main()
+        {
+            numberOfElements = int.Parse(Console.ReadLine());
+            slotsToFill = int.Parse(Console.ReadLine());
+
+            combinations = new int[slotsToFill];
+
+            FindCombinations(0, 1);
+        }
+
+        private static void FindCombinations(int combinationIndex, int startIndex)
+        {
+            if (combinationIndex >= slotsToFill)
+            {
+                Console.WriteLine(string.Join(" ", combinations));
+                return;
+            }
+
+            for (int number = startIndex; number <= numberOfElements; number++)
+            {
+                combinations[combinationIndex] = number;
+
+                FindCombinations(combinationIndex + 1, number + 1);
+            }
+        }
+    }
+}
