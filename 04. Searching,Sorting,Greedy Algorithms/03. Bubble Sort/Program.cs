dbe83550613c6f2ce0a74530ac008a964f2cfcc5
BorisLechev <b.k.lechev@gmail.com>
2022-02-10 23:48:26 +0200

    Add 03. Bubble Sort.

---
+++ 04. Searching,Sorting,Greedy Algorithms/03. Bubble Sort/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Bubble_Sort
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+
+            BubbleSort(input);
+
+            Console.WriteLine(string.Join(" ", input));
+        }
+
+        private static void BubbleSort(int[] numbers)
+        {
+            var isSorted = false;
+            var sortedElementsCount = 0;
+
+            while (isSorted == false)
+            {
+                isSorted = true;
+
+                for (int i = 1; i < numbers.Length - sortedElementsCount; i++)
+                {
+                    if (numbers[i - 1] > numbers[i])
+                    {
+                        Swap(numbers, i - 1, i);
+                        isSorted = false;
+                    }
+                }
+
+                sortedElementsCount += 1;
+            }
+        }
+
+        private static void Swap(int[] numbers, int firstIndex, int secondIndex)
+        {
+            var temp = numbers[firstIndex];
+            numbers[firstIndex] = numbers[secondIndex];
+            numbers[secondIndex] = temp;
+        }
+    }
+}
