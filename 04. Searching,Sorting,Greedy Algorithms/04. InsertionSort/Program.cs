243e44c1ba8720adf035cf008271842683366c8a
BorisLechev <b.k.lechev@gmail.com>
2022-02-11 12:39:21 +0200

    Add 04. Insertion Sort.

---
+++ 04. Searching,Sorting,Greedy Algorithms/04. InsertionSort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._InsertionSort
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+
+            InsertionSort(numbers);
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        private static void InsertionSort(int[] numbers)
+        {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                var j = i;
+
+                while (j > 0 && numbers[j - 1] > numbers[j])
+                {
+                    Swap(numbers, j - 1, j);
+                    j -= 1;
+                }
+            }
+        }
+
+        private static void Swap(int[] numbers, int firstIndex, int secondIndex)
+        {
+            var temp = numbers[firstIndex];
+            numbers[firstIndex] = numbers[secondIndex];
+            numbers[secondIndex] = temp;
+        }
+    }
+}
