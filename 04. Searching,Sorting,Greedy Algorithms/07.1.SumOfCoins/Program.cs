f36b3d15e808747b7dfce15a3a160bb7eaa90841
BorisLechev <b.k.lechev@gmail.com>
2022-09-06 21:01:38 +0300

    Add Sum of coins Variant II

---
+++ 04. Searching,Sorting,Greedy Algorithms/07.1.SumOfCoins/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _07._1.SumOfCoins
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                        .Split(", ", StringSplitOptions.None)
+                        .Select(int.Parse)
+                        .OrderByDescending(c => c)
+                        .ToArray();
+
+            var coins = new Queue<int>(input);
+
+            var target = int.Parse(Console.ReadLine());
+            var usedCoins = new Dictionary<int, int>();
+            var totalCoins = 0;
+
+            while (target > 0 && coins.Count > 0)
+            {
+                var currentCoin = coins.Dequeue();
+                var count = target / currentCoin;
+
+                if (count == 0)
+                {
+                    continue;
+                }
+
+                usedCoins[currentCoin] = count;
+                totalCoins += count;
+
+                target %= currentCoin;
+            }
+
+            if (target == 0)
+            {
+                Console.WriteLine($"Number of coins to take: {totalCoins}");
+
+                foreach (var usedCoin in usedCoins)
+                {
+                    Console.WriteLine($"{usedCoin.Value} coin(s) with value {usedCoin.Key}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
